0d3e59b30efd88357e7c91b2696bdf002dff462e
LANGAN84 <langan.kg@gmail.com>
2021-11-24 14:47:06 +0000

    test clean up'

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,26 +1,23 @@
 require 'account'
 
 describe "Account" do
+  let(:account) { Account.new }
   it 'is initialised with an opening default balance' do
-    account = Account.new
     expect(account.balance).to be(0)
   end
 
   it 'can deposit funds to the account' do
-    account = Account.new
     account.deposit_funds(100)
     expect(account.balance).to be(100)
   end
 
   it 'can withdrawn funds from the account' do
-    account = Account.new
     account.deposit_funds(100)
     account.withdraw_funds(20)
     expect(account.balance).to be(80)
   end
 
   it 'can store an instance of a transaction' do
-    account = Account.new
     account.deposit_funds(100)
     account.withdraw_funds(50)
     expect(account.transaction_history.length).to eq 2
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,25 +1,22 @@
 require 'transaction'
 
 describe 'Transaction' do
+  let(:transaction) { Transaction.new(100, 0, 100) }
 
   it 'records the date the transaction took place' do
     @time_now = Time.now.strftime('%d/%m/%Y')
-    transaction = Transaction.new(100, 0, 100)
     expect(transaction.date).to eq(@time_now)
   end
 
   it 'records the credit in the transaction' do
-    transaction = Transaction.new(100, 0, 100)
     expect(transaction.credit).to eq 100
   end
 
   it 'records the debit in the transaction' do
-    transaction = Transaction.new(100, 0, 100)
     expect(transaction.debit).to eq 0
   end
 
   it 'records the balance after the transaction' do
-    transaction = Transaction.new(100, 0, 100)
     expect(transaction.balance).to eq 100
   end
 end
